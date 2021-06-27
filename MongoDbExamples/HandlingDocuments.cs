dccd5cb21a204160077d25f07b32ec6c5f284db8
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-06-27 12:37:49 -0300

    Creating a json document to MongoDb using BsonDocument class

---
+++ MongoDbExamples/HandlingDocuments.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Threading.Tasks;
+
+namespace MongoDbExamples
+{
+    public class HandlingDocuments
+    {
+        static void Main(string[] args)
+        {
+            Task T = HandlingDocumentsAsync(args);
+            Console.WriteLine("Press ENTER");
+            Console.ReadLine();
+        }
+
+        private static async Task HandlingDocumentsAsync(string[] args)
+        {
+            var doc = new BsonDocument("Title", "Game of Thrones");
+            doc.Add("Author", "George R R Martins");
+            doc.Add("Year", 1999);
+            doc.Add("Pages", 855);
+
+            var aboutArray = new BsonArray();
+
+            aboutArray.Add("Fantasy");
+            aboutArray.Add("Action");
+
+            doc.Add("About", aboutArray);
+
+            Console.WriteLine(doc);
+        }
+    }
+}
