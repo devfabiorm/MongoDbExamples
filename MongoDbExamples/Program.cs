8a892b0bd9328a92debce15dbefaea3a9d7f3e6d
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-06-27 12:01:00 -0300

    Add project files.

---
+++ MongoDbExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MongoDbExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
